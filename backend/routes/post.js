68a9cc79ccbf7f74980eca65fd7f6f6caeceeef5
agatazz <a.zabuska.557@studms.ug.edu.pl>
2021-01-29 17:48:03 +0100

    Update post.js

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -4,18 +4,14 @@ const router=express.Router();
 
 
 const {create,list,read,update,remove}=require('../controllers/post');
-//const {requireLogin} =require('../controllers/auth');
+const {requireLogin} =require('../controllers/auth');
 
-router.post('/post', create);
+router.post('/post',requireLogin, create);
 router.get('/posts', list);
 router.get('/post/:slug',read);
-router.put('/post/:slug',update);
-router.delete('/post/:slug',remove);
+router.put('/post/:slug',requireLogin,update);
+router.delete('/post/:slug',requireLogin,remove);
 
-// router.get('/secret',requireLogin,(req,res) =>{
-//     res.json({
-//         data:req.user.name
-//     });
-// });
+ 
 
 module.exports=router;
